230e0c88d4288671bcef4eab4dbf71a89d67e6b4
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-02-28 12:33:33 -0300

    refactor(neo-tree): remove git status integration
    Removed the git_status source and related keybindings, symbols, and
    configuration from the neo-tree plugin setup. This simplifies the
    explorer to only use the filesystem source and removes the git status
    float and winbar source selector.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,9 +4,8 @@ return {
     branch       = "v3.x",
     cmd          = "Neotree",
     keys         = {
-      { "<leader>e", "<cmd>Neotree toggle<cr>",                  desc = "Explorer (root)" },
-      { "<leader>E", "<cmd>Neotree toggle dir=%:p:h<cr>",        desc = "Explorer (file dir)" },
-      { "<leader>ge", "<cmd>Neotree float git_status<cr>",       desc = "Git status (neo-tree)" },
+      { "<leader>e", "<cmd>Neotree toggle<cr>",           desc = "Explorer (root)" },
+      { "<leader>E", "<cmd>Neotree toggle dir=%:p:h<cr>", desc = "Explorer (file dir)" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -15,10 +14,7 @@ return {
     },
     opts = {
       close_if_last_window = true,
-      sources = { "filesystem", "git_status" },
-      source_selector = {
-        winbar = true,
-      },
+      sources = { "filesystem" },
       filesystem = {
         bind_to_cwd       = false,
         follow_current_file = { enabled = true },
@@ -33,7 +29,7 @@ return {
       window = {
         width = 35,
         mappings = {
-          ["<space>"] = "none", -- don't shadow leader
+          ["<space>"] = "none",
           ["l"]       = "open",
           ["h"]       = "close_node",
         },
@@ -44,19 +40,6 @@ return {
           folder_closed = "",
           folder_open   = "",
           folder_empty  = "󰜌",
-        },
-        git_status = {
-          symbols = {
-            added     = "",
-            modified  = "",
-            deleted   = "✖",
-            renamed   = "󰁕",
-            untracked = "",
-            ignored   = "",
-            unstaged  = "󰄱",
-            staged    = "",
-            conflict  = "",
-          },
         },
       },
     },
